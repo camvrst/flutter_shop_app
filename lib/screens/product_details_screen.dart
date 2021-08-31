1f1ac7b514fddeb78f03f5fa5c16d3128eda7fd6
Camillle Verriest <camille.verriest@p3lab.com>
2021-08-31 16:55:33 +0200

    added product details page design

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -16,13 +16,46 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
     // context = communication channel
-    // listen = false means that it won't rebuild if the data from the provider change. 
+    // listen = false means that it won't rebuild if the data from the provider change.
     final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
         .findById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+              style: TextStyle(fontFamily: 'Lato'),
+          
+            ),),
+          ],
+        ),
       ),
     );
   }
